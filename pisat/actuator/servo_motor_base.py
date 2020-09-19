95dd5ebc00052d5897f50ed0b7529bb0f64686ff
jjj999 <spaceship2021@gmail.com>
2020-09-19 20:58:30 +0900

    reconfigure actuator subpackage

---
+++ pisat/actuator/servo_motor_base.py
@@ -0,0 +1,25 @@
+
+from enum import Enum
+from typing import Union
+
+from pisat.actuator.motor_driver_base import MotorDriverBase
+
+
+class ServoMotorBase(MotorDriverBase):
+    
+    # TODO to be overrided
+    class Angle(Enum):
+        MAX = 0.
+        MIN = 0.
+        
+    @classmethod
+    def is_valid_angle(cls, angle: Union[int, float]) -> bool:
+        if cls.Angle.MIN.value <= angle <= cls.Angle.MAX.value:
+            return True
+        else:
+            return False
+        
+    # TODO to be overrided
+    def to_angle(self, angle: Union[int, float]) -> None:
+        pass
+        
