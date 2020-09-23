a978168ca1a60ee1f8334317de5eaf63e06fac5d
jjj999 <spaceship2021@gmail.com>
2020-09-23 19:14:42 +0900

    remove im920 from dependancies

--- setup.py
+++ setup.py
@@ -1,4 +1,36 @@
+
 from setuptools import setup, find_packages
+
+
+def is_raspberry_pi() -> bool:
+
+    try:
+        with open('/proc/cpuinfo', 'r') as cpuinfo:
+            found = False
+            for line in cpuinfo:
+                if line.startswith('Hardware'):
+                    found = True
+                    label, value = line.strip().split(':', 1)
+                    value = value.strip()
+                    if value not in (
+                        'BCM2708',
+                        'BCM2709',
+                        'BCM2835',
+                        'BCM2836'
+                    ):
+                        return False
+            if not found:
+                return False
+    except IOError:
+        return False
+
+    return True
+
+
+install_requires = ["numpy", "pyserial"]
+if is_raspberry_pi():
+    install_requires.append("pigpio")
+    install_requires.append("RPi.GPIO")
 
 setup(
     name="pisat",
@@ -8,9 +40,5 @@ setup(
     author_email="spaceship2021@gmail.com",
     license=license,
     packages=find_packages(exclude=("docs", "tests", "sample")),
-    install_requires=[
-        "numpy",
-        "pigpio",
-        "pyserial"
-    ]
+    install_requires=install_requires
 )
