18df62f52be5dec82871e5dbf25c39882303342d
jjj999 <spaceship2021@gmail.com>
2020-10-27 17:02:08 +0900

    enabled to select default voltage value at initilization of DigitalOutputHandlerBase

--- pisat/handler/digital_output_handler_base.py
+++ pisat/handler/digital_output_handler_base.py
@@ -1,8 +1,18 @@
 
+from typing import Optional
 from pisat.handler.digital_io_handler_base import DigitalIOHandlerBase
 
 
 class DigitalOutputHandlerBase(DigitalIOHandlerBase):
+    
+    def __init__(self,
+                 pin: int,
+                 default: bool = False,
+                 name: Optional[str] = None) -> None:
+        super().__init__(pin, name=name)
+        
+        if default:
+            self.set_high()
     
     def set_high(self) -> None:
         pass
