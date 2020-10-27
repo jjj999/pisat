494b4f0372263b7ebacbc390a3fedf99a9c2c65b
jjj999 <spaceship2021@gmail.com>
2020-10-27 16:33:12 +0900

    add new gps module named SAM-M8Q

--- pisat/sensor/sam_m8q.py
+++ pisat/sensor/sam_m8q.py
@@ -1,6 +1,48 @@
 
-from pisat.sensor.sensor_base import SensorBase
+from typing import Dict, List, Optional, Tuple
+
+from pisat.config.type import Logable
+from pisat.handler.i2c_handler_base import I2CHandlerBase
+from pisat.handler.serial_handler_base import SerialHandlerBase
+from pisat.handler.handler_base import HandlerBase
+from pisat.sensor.sensor_base import HandlerMismatchError, SensorBase
+from pisat.sensor.serial_gps import SerialGPS
 
 
-class SamM8Q(SensorBase):
+class UARTSamM8Q(SerialGPS):
     pass
+
+
+class I2CSamM8Q(SensorBase):
+    pass
+
+
+# TODO Confirm UART signals
+class SamM8Q(SensorBase):
+    
+    def __init__(self,
+                 handler: Optional[HandlerBase] = None,
+                 debug: bool = False,
+                 name: Optional[str] = None) -> None:
+        super().__init__(handler=handler, debug=debug, name=name)
+        
+        if debug:
+            return
+        
+        if isinstance(handler, SerialHandlerBase):
+            self._base = UARTSamM8Q(handler=handler)
+        elif isinstance(handler, I2CHandlerBase):
+            self._base = I2CSamM8Q(handler=handler)
+        else:
+            raise HandlerMismatchError(
+                "'handler' must be for UART or I2C."
+            )
+            
+    # TODO
+    def read(self, *dnames: Tuple[str, ...]) -> Dict[str, Logable]:
+        pass
+    
+    # TODO
+    def readf(self, *dnames: Tuple[str, ...]) -> List[Logable]:
+        pass
+        
