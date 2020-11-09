d1becab71e0a7a20ebc5a3d0e1c15be23023d57f
jjj999 <spaceship2021@gmail.com>
2020-11-09 19:38:52 +0900

    remove all shortcuts

--- pisat/util/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from pisat.util.about_time import get_time_stamp
-from pisat.util.platform import is_raspberry_pi
-from pisat.util.cached_property import cached_property
-from pisat.util.gps_parser import GPSParser
-from pisat.util.type import is_all_None
-from pisat.util.deco import restricted_setter, restricted_range_setter
