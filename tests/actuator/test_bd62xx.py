1da91ca8d67d774a07e45ddb84defd56bbd0e230
jjj999 <spaceship2021@gmail.com>
2020-11-15 21:52:48 +0900

    update test scripts about motor

--- tests/actuator/test_bd62xx.py
+++ tests/actuator/test_bd62xx.py
@@ -8,33 +8,46 @@ from pisat.actuator import BD62xx
 from pisat.handler import PigpioPWMHandler
 
 
-PIN_FIN = 13
-NAME_FIN = "fin"
-PIN_RIN = 19
-NAME_RIN = "rin"
+PIN_L_FIN = 12
+NAME_L_FIN = "l_fin"
+PIN_L_RIN = 18
+NAME_L_RIN = "l_rin"
+PIN_R_FIN = 16
+NAME_R_FIN = "r_fin"
+PIN_R_RIN = 26
+NAME_R_RIN = "r_rin"
 FREQUENCY = 40000
-
 
 class TestBD62xx(unittest.TestCase):
     
     def setUp(self) -> None:
         pi = pigpio.pi()
-        handler_fin = PigpioPWMHandler(pi, PIN_FIN, FREQUENCY, name=NAME_FIN)
-        handler_rin = PigpioPWMHandler(pi, PIN_RIN, FREQUENCY, name=NAME_RIN)
+        handler_L_fin = PigpioPWMHandler(pi, PIN_L_FIN, FREQUENCY, name=NAME_L_FIN)
+        handler_L_rin = PigpioPWMHandler(pi, PIN_L_RIN, FREQUENCY, name=NAME_L_RIN)
+        handler_R_fin = PigpioPWMHandler(pi, PIN_R_FIN, FREQUENCY, name=NAME_R_FIN)
+        handler_R_rin = PigpioPWMHandler(pi, PIN_R_RIN, FREQUENCY, name=NAME_R_RIN)
 
-        self.driver = BD62xx(handler_fin, handler_rin, name="driver")
+        self.driver_L = BD62xx(handler_L_fin, handler_L_rin, name="driverL")
+        self.driver_L.brake()
+        self.driver_R = BD62xx(handler_R_fin, handler_R_rin, name="driverR")
         
     def test_ccw(self):
-        for i in range(1000):
-            self.driver.ccw(i / 10)
-            time.sleep(0.05)
-        self.driver.brake()
+        print("test ccw")
+        for i, j in zip(range(500), range(500, 1000)):
+            self.driver_L.ccw(i / 10)
+            self.driver_R.cw(j / 10)
+            time.sleep(0.01)
+        self.driver_L.brake()
+        self.driver_R.brake()
         
     def test_cw(self):
+        print("test cw")
         for i in range(1000):
-            self.driver.cw(i / 10)
-            time.sleep(0.05)
-        self.driver.brake()
+            self.driver_L.cw(i / 10)
+            self.driver_R.ccw(i / 10)
+            time.sleep(0.01)
+        self.driver_L.brake()
+        self.driver_R.brake()
         
         
 if __name__ == "__main__":
--- tests/actuator/test_two_wheels.py
+++ tests/actuator/test_two_wheels.py
@@ -12,9 +12,9 @@ PIN_L_FIN = 12
 NAME_L_FIN = "l_fin"
 PIN_L_RIN = 18
 NAME_L_RIN = "l_rin"
-PIN_R_FIN = 13
+PIN_R_FIN = 16
 NAME_R_FIN = "r_fin"
-PIN_R_RIN = 19
+PIN_R_RIN = 26
 NAME_R_RIN = "r_rin"
 FREQUENCY = 40000
 
@@ -23,12 +23,46 @@ class TestTwoWheels(unittest.TestCase):
     
     def setUp(self) -> None:
         pi = pigpio.pi()
-        handler_l_fin = PigpioPWMHandler(pi, PIN_L_FIN, FREQUENCY, name=NAME_L_FIN)
-        handler_l_rin = PigpioPWMHandler(pi, PIN_L_RIN, FREQUENCY, name=NAME_L_RIN)
-        handler_r_fin = PigpioPWMHandler(pi, PIN_R_FIN, FREQUENCY, name=NAME_R_FIN)
-        handler_r_rin = PigpioPWMHandler(pi, PIN_R_RIN, FREQUENCY, name=NAME_R_RIN)
+        handler_L_fin = PigpioPWMHandler(pi, PIN_L_FIN, FREQUENCY, name=NAME_L_FIN)
+        handler_L_rin = PigpioPWMHandler(pi, PIN_L_RIN, FREQUENCY, name=NAME_L_RIN)
+        handler_R_fin = PigpioPWMHandler(pi, PIN_R_FIN, FREQUENCY, name=NAME_R_FIN)
+        handler_R_rin = PigpioPWMHandler(pi, PIN_R_RIN, FREQUENCY, name=NAME_R_RIN)
+
+        self.driver_L = BD62xx(handler_L_fin, handler_L_rin, name="driverL")
+        self.driver_R = BD62xx(handler_R_fin, handler_R_rin, name="driverR")
+        self.wheels = TwoWheels(self.driver_L, self.driver_R, name="wheels")
+
+    def test_straight(self):
+        self.wheels.straight(30)
+        time.sleep(3)
+        self.wheels.straight(50)
+        time.sleep(3)
+        self.wheels.straight(70)
+        time.sleep(3)
+        self.wheels.brake()
+
+    def test_ccw(self):
+        print("test_ccw")
+        for i in range(100):
+            self.wheels.ccw(i)
+            time.sleep(0.1)
+        self.wheels.brake()
         
-        driver_L = BD62xx(handler_l_fin, handler_l_rin, "left")
-        driver_R = BD62xx(handler_r_fin, handler_r_rin, "right")
-        self.wheels = TwoWheels(driver_L, driver_R, name="wheels")
+    def test_cw(self):
+        print("test_cw")
+        for i in range(100):
+            self.wheels.cw(i)
+            time.sleep(0.1)
+        self.wheels.brake()
         
+    def test_curve_ccw(self):
+        print("test_curve_ccw")
+        self.wheels.straight(100)
+        time.sleep(3)
+        self.wheels.curve_ccw(50)
+        time.sleep(5)
+        self.wheels.brake()
+
+        
+if __name__ == "__main__":
+    unittest.main()
